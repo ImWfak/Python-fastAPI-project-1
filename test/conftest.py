3ed575ba6b4c523a6ccf79468b0479235e85f0fe
Latitude 7490 <borysevychalex@gmail.com>
2026-04-12 22:42:29 +0300

    fix: Fixed different event loop collision in conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,12 +1,7 @@
 import os
-import pytest
 import pytest_asyncio
 from tortoise import Tortoise
 from dotenv import load_dotenv
-from httpx import AsyncClient, ASGITransport
-
-from main import app
-from product.product_model import ProductModel
 
 load_dotenv(".test.env")
 
@@ -17,10 +12,8 @@ PASSWORD = os.getenv("PASSWORD")
 DATABASE = os.getenv("DATABASE")
 
 
-@pytest_asyncio.fixture(scope="session", autouse=True)
-async def connect_test_db():
-    await Tortoise.close_connections()
-
+@pytest_asyncio.fixture(scope="function", autouse=True)
+async def db():
     await Tortoise.init({
         "connections": {
             "default": f"postgres://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
@@ -38,24 +31,10 @@ async def connect_test_db():
 
     yield
 
-    connection = Tortoise.get_connection("default")
-    await connection.execute_script("DROP SCHEMA public CASCADE;")
-    await connection.execute_script("CREATE SCHEMA public;")
-
+    await Tortoise.get_connection("default").execute_script(
+        """
+        DROP SCHEMA public CASCADE;
+        CREATE SCHEMA public;
+        """
+    )
     await Tortoise.close_connections()
-
-
-@pytest_asyncio.fixture(autouse=True)
-async def clean_db():
-    await ProductModel.all().delete()
-    yield
-    await ProductModel.all().delete()
-
-
-"""@pytest_asyncio.fixture
-async def client():
-    async with AsyncClient(
-            transport=ASGITransport(app=app),
-            base_url="http://test"
-    ) as async_client:
-        yield async_client"""
